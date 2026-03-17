614009d72b9469d2f0232bd3213220dc513234c2
manovares <rm566844@fiap.com.br>
2026-03-17 11:58:18 -0300

    Terminando exercicio polimorfismo sobrecarga

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class main{
+public class Main {
     public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        c.reagir("Olá");
+        c.reagir("Vai apanhar");
 
+        c.reagir(11,45);
+        c.reagir(19,00);
+        c.reagir(true);
     }
-}
 
+}
